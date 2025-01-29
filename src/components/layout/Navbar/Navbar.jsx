9e87400b20b556443fa040f034ecb38e016cd240
Anita Wangui <anitawangui22@gmail.com>
2025-01-30 01:50:34 +0300

    add logout feature

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,13 +1,26 @@
 import "./Navbar.css";
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import  React, {useState, useEffect} from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 
-function Navbar(){
+const Navbar=()=>{
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate=useNavigate();
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            setIsLoggedIn(true);
+        }
+    }, []);
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        setIsLoggedIn(false);
+        navigate('/');
+    }
     return (
         <nav className="navbar">
             <div className="container-fluid">
                 <div className="logo">
-                    <h4>Wander<span>With</span></h4>
+                    <h4>Wander<span>Soul</span></h4>
                 </div>
                 <div className="nav">
                     <NavLink to="/" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>Home</NavLink>
@@ -16,8 +29,18 @@ function Navbar(){
                     <NavLink to="/guide" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>Guides</NavLink>
                 </div>
                 <div className="links">
-                    <NavLink to="/login" className={({ isActive }) => isActive ? "active" : ""}>Login</NavLink>
-                    <NavLink to="/signup" className={({ isActive }) => isActive ? "active" : ""}>Sign Up</NavLink>
+                    {isLoggedIn ? (
+                        <>
+                            <NavLink to="/profile" className={({ isActive }) => isActive ? "active" : ""}>Profile</NavLink>
+                            <button onClick={handleLogout}>Logout</button>
+                        </>
+                    ) : (
+                        <>
+                            <NavLink to="/login" className={({ isActive }) => isActive ? "active" : ""}>Login</NavLink>
+                            <NavLink to="/signup" className={({ isActive }) => isActive ? "active" : ""}>Sign Up</NavLink>
+                        </>
+                    )}
+                   
                 </div>
             </div>
         </nav>
